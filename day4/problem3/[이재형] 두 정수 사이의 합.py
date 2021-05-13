16e79bbe6f00baa840a16c2d7b20393a4912e6ef
lee-jae-hyeong <76590396+lee-jae-hyeong@users.noreply.github.com>
2021-05-13 21:50:32 +0900

    Add files via upload

---
+++ day4/problem3/[이재형] 두 정수 사이의 합.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    if a == b :
+        return a
+    else :
+        if a < b :
+            return sum(i for i in range(a, b+1))
+        else :
+            return sum( i for i in range(b, a+1))
