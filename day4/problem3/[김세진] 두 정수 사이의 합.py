c900d9d4b554e8de6e9304c8ff2c61a05c8a3ca9
Sejin <63702924+threegenie@users.noreply.github.com>
2021-05-13 22:36:59 +0900

    Update [김세진] 두 정수 사이의 합.py

--- day4/problem3/[김세진] 두 정수 사이의 합.py
+++ day4/problem3/[김세진] 두 정수 사이의 합.py
@@ -1,3 +1,6 @@
+# 1부터 n까지 더하는 공식 : n/2*(n+1)
+# a부터 b까지의 숫자들을 다 더하려면, 1부터 b까지 전부다 더한 값에서 1부터 a까지 전부다 더한 값을 빼서 거기다 a를 더하면 됨
+
 def solution(a,b):
   answer = 0
   if a==b:
@@ -7,3 +10,4 @@ def solution(a,b):
   else:
     answer = int((a/2*(a+1))-(b/2*(b+1))+b)
   return answer
+
