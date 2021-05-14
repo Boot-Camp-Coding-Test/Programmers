5bf19db57d97c0b1e9e11924d8bfd8d21d21c174
joonghoonc <joonghoon.choi@icloud.com>
2021-05-14 23:19:31 +0900

    시간초과!!!! 불합격!!ㅠㅠ

---
+++ day5/problem4/[최중훈] 소수 찾기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    
+    if n == 1:
+        return answer
+    
+    for i in range(2, n+1):
+        tmp = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                tmp += 1
+        
+        if tmp == 2:
+            answer += 1
+
+################### 시간초과로 통과 못함 내일 다시..#####################    
