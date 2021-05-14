6cdd29d6ad69704955f4244bc426807d97ab5e79
BoHye0202 <55703579+BoHye0202@users.noreply.github.com>
2021-05-14 18:53:43 +0900

    Create [최보혜]2016.py

---
+++ day4/problem4/[최보혜]2016.py
@@ -0,0 +1,5 @@
+def solution(a, b):
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    total = sum(month[:a-1]) + b
+    return days[total%7-1]
