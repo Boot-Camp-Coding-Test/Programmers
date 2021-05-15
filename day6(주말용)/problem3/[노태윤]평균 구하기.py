3339b7f75de60658367b60c9fa5b55d4f12708a8
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2021-05-15 17:03:14 +0900

    Update [노태윤]평균 구하기.py

--- day6(주말용)/problem3/[노태윤]평균 구하기.py
+++ day6(주말용)/problem3/[노태윤]평균 구하기.py
@@ -7,8 +7,8 @@ def solution(arr):
     # np.mean 활용
     mean_value = np.mean(numpy_array)
     
-    # 만약 소수점 자리 끝이 0 이면 int 형으로 변환
-    if str(mean_value)[-1] == 0:
+    # 만약 소수점 자리 끝이 '0' 이면 int 형으로 변환
+    if str(mean_value)[-1] == '0':
         answer = int(mean_value)
         
     # 나머지는 그냥 float 형으로 반환
