fa67332a0bc94e6578b5049365d3683ce70baad3
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2021-05-15 15:21:12 +0900

    Update [노태윤]모의고사 도움말 (런타임 에러).py

--- day4/problem5/[노태윤]모의고사 도움말 (런타임 에러).py
+++ day4/problem5/[노태윤]모의고사 도움말 (런타임 에러).py
@@ -43,3 +43,69 @@ def solution(answers):
     answer = sorted(answer)
 
     return answer
+
+# =======================================================================================
+# 재도전
+
+def solution(answers):
+    answer = []
+    
+    # 패턴 리스트로 담기
+    a = [1,2,3,4,5]
+    b = [2,1,2,3,2,4,2,5]
+    c = [3,3,1,1,2,2,4,4,5,5]
+
+    # 길이 조절
+    a_divided = len(answers)//len(a)
+    b_divided = len(answers)//len(b)
+    c_divided = len(answers)//len(c)
+
+    a.extend(a_divided*a)
+    b.extend(b_divided*b)
+    c.extend(c_divided*c)
+
+    new_dict = {
+        1:0,
+        2:0,
+        3:0
+    }
+
+    # answers 에 담긴 숫자 == 패턴 이면 아래 실행
+    for i in range(len(answers)):
+        if a[i] == answers[i]  :
+            new_dict[1]+=1
+        if b[i] == answers[i] :
+            new_dict[2]+=1
+        if c[i] == answers[i] :
+            new_dict[3]+=1
+    
+    # max 값에 해당하는 key 값 리스트로 담고 오름차순 정렬
+    answer = sorted([a[0] for a in new_dict.items() if a[1]==max(new_dict.values())])
+
+    return answer
+
+# Key Points
+    # 처음에는 
+    '''
+    for i in range(len(answers)):
+        if answers[i] == a[i] :
+            new_dict[1]+=1
+        if answers[i] == b[i] :
+            new_dict[2]+=1
+        if answers[i] == c[i] :
+            new_dict[3]+=1
+    '''
+    # 이렇게 풀었음
+    
+    # 이러면 if 문 중 하나라도 True 면 if 문 실행하고 다시 for loop 돌게 됨
+    
+    '''
+    for i in range(len(answers)):
+        if a[i] == answers[i]  :
+            new_dict[1]+=1
+        if b[i] == answers[i] :
+            new_dict[2]+=1
+        if c[i] == answers[i] :
+            new_dict[3]+=1
+    '''
+    # 이렇게 반대로 작성해야 함!
