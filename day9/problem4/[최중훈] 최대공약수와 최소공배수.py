d6951b29d09238626f9776cfc640ae668d68584f
joonghoonc <joonghoon.choi@icloud.com>
2021-05-19 18:00:56 +0900

    Update [최중훈] 최대공약수와 최소공배수.py

--- day9/problem4/[최중훈] 최대공약수와 최소공배수.py
+++ day9/problem4/[최중훈] 최대공약수와 최소공배수.py
@@ -1,5 +1,4 @@
 def solution(n, m):
-  
     def gcd(n, m): # 최대공약수 찾는 재귀함수
         if m == 0:
             return n
@@ -7,12 +6,8 @@ def solution(n, m):
             (n, m) == (m, n)
         return gcd(m, n%m)
 
-    if n == m: # 두 수가 같은 경우는 그 수가 gcd, lcm
-        gcd_num = n
-        lcm_num = n
-    else:
-        gcd_num = int(gcd(n, m)) # 최대공약수 함수를 사용해서 구한 gcd
-        lcm_num = int(n*m / gcd_num) # 두 수를 곱하고 gcd로 나누어주면 lcm
+    gcd_num = int(gcd(n, m)) # 최대공약수 함수를 사용해서 구한 gcd
+    lcm_num = int(n*m / gcd_num) # 두 수를 곱하고 gcd로 나누어주면 lcm
     answer = [gcd_num, lcm_num]
 
     return answer
