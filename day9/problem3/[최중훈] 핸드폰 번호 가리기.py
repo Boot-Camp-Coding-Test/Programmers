91bc8093919c4982a51b388da9bf5d518784821a
joonghoonc <joonghoon.choi@icloud.com>
2021-05-19 17:24:05 +0900

    Update [최중훈] 핸드폰 번호 가리기.py

--- day9/problem3/[최중훈] 핸드폰 번호 가리기.py
+++ day9/problem3/[최중훈] 핸드폰 번호 가리기.py
@@ -9,3 +9,8 @@ def solution(phone_number):
         answer += n
     
     return answer
+
+
+def solution(phone_number):
+    answer = '*'*(len(phone_number)-4) + phone_number[-4:]
+    return answer
