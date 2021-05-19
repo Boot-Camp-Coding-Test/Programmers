efa202cc0c9b14bc698d59c34574fd6f1e7c16c7
C-Designer <c.designer.dev@gmail.com>
2021-05-19 19:45:36 +0900

    Update [김창동] 하샤드 수.py

--- day9/problem5/[김창동] 하샤드 수.py
+++ day9/problem5/[김창동] 하샤드 수.py
@@ -1,8 +1,8 @@
-# 공간 복잡도
-def solution(x):
-    return not bool(x % eval('+'.join(list(str(x)))))
-
 def solution(x):
     str_ = list(str(x))   # ['1', '5', '7']
     ev = eval('+'.join(str_)) # eval('1 + 5 + 7') => 13
     return not bool(x % ev) # 나누어 떨어지면 0으로 반환 => bool함수에 False => not연산자로 True로 변환
+
+# 공간 복잡도
+def solution(x):
+    return not bool(x % eval('+'.join(list(str(x)))))
