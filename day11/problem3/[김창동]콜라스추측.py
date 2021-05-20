1a2035d7c94b7fd23627737c85328237925220b9
C-Designer <c.designer.dev@gmail.com>
2021-05-20 20:34:46 +0900

    Create [김창동]콜라스추측.py

---
+++ day11/problem3/[김창동]콜라스추측.py
@@ -0,0 +1,10 @@
+def solution(num, cnt=0):
+    if cnt > 500:
+        return -1
+    if num == 1:
+        return cnt
+    
+    if num % 2: # 홀수일때
+        return solution(num * 3 + 1, cnt + 1)
+    else:
+        return solution(num // 2, cnt + 1)
