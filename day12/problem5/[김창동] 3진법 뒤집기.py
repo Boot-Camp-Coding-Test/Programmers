df20900fb86b20c8eb70704d01402dbe48b090fa
C-Designer <c.designer.dev@gmail.com>
2021-05-22 11:37:40 +0900

    Update [김창동] 3진법 뒤집기.py

--- day12/problem5/[김창동] 3진법 뒤집기.py
+++ day12/problem5/[김창동] 3진법 뒤집기.py
@@ -1,3 +1,4 @@
+# 재귀 사용
 def solution(n):
     three = numeral_system(n, 3)
     return int(three[::-1], 3)
@@ -6,3 +7,14 @@ def numeral_system(number, base):
     q, r = divmod(number, base)
     n = '0123456789ABCDEF'[r]
     return numeral_system(q, base) + n if q else n
+
+#######################################################
+
+# 재귀 사용x
+def solution(num):
+    answer = ''
+    
+    while num >= 1:
+        num, rest = divmod(num, 3)
+        answer += str(rest)
+    return int(answer[::-1])
