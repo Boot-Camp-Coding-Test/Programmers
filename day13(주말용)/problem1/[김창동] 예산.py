e4b63f004acfb03031e97f311dc893e88f3a3c96
C-Designer <c.designer.dev@gmail.com>
2021-05-23 00:00:24 +0900

    Update [김창동] 예산.py

--- day13(주말용)/problem1/[김창동] 예산.py
+++ day13(주말용)/problem1/[김창동] 예산.py
@@ -8,3 +8,18 @@ def solution(d, budget):
         d = d[:-1]
         
     return len(d)
+
+
+# 삽질흔적 / 시간초과
+from itertools import combinations
+
+def solution(d, budget):
+    
+    for n in range(len(d)):
+        team = list(set(combinations(d, len(d) - n)))
+        team = [sum(i) for i in team]
+        
+        if budget >= min(team):
+            return len(d) - n
+            
+    return 0
